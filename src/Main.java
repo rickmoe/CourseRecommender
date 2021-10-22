bbe6c6b1d939d4fda80dc05ee20edeb32671419a
rickmoe <Cosmic.Star@comcast.net>
2021-10-21 23:51:38 -0500

    Added CSV Files, Basic Pseudocode, and Began Entering Data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        // Take in input of majors and classes taken
 
+        // Construct graph based on majors and classes taken
+
+        // Remove courses that must be taken and can be directly taken, iterate until none found
+
+        // Determine pre-requisite combinations to satisfy remaining requirements and add them
+        // to a list
+
+        // Evaluate credit count for all pre-requisite combinations
+        // Keep track of the minimum then remove any too high above the min
+
+        // Sort remaining combos based on credit count
+
+        // Present results to user
+        
     }
 }
