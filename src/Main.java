d5321264b690060b04f665c8fc5cc913776fddcd
rickmoe <Cosmic.Star@comcast.net>
2021-10-22 01:37:56 -0500

    Added a simple system to get user input

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,45 @@
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        LinkedList<LinkedList<String>> fileLists = CSVReader.readCSV("res/majors.csv");
-        for (LinkedList<String> lineList : fileLists) {
-            System.out.println();
-            for (String value : lineList) {
-                LinkedList<String> splitVals = CSVReader.parseCSVLine(value, ";");
-                System.out.print("- " + splitVals.get(0));
-                if (splitVals.size() > 1) System.out.print(" : \t" + splitVals.get(1));
-                System.out.println();
+        // Take in input of majors and classes taken
+        Scanner scnr = new Scanner(System.in);
+        String userInput = "";
+        LinkedList<LinkedList<String>> majorsList = CSVReader.readCSV("res/majors.csv");
+        LinkedList<String> headers = majorsList.get(0);
+        LinkedList<String> majorData = null;
+        LinkedList<String> classesTaken = new LinkedList<>();
+
+        System.out.println("Please enter your major: ");
+        while (majorData == null) {
+            userInput = scnr.nextLine();
+            for (LinkedList<String> data : majorsList) {
+                if (data.equals(headers)) continue;
+                if (userInput.toLowerCase().equals(data.get(headers.indexOf("major")).toLowerCase()))
+                    majorData = data;
             }
+            if (majorData == null) System.out.println("Please enter a valid major in the database: ");
         }
 
-        // Take in input of majors and classes taken
+        while (!userInput.equals("esc")) {
+            System.out.println("Please enter the classes you've taken. Enter \"esc\" to end the list.");
+            userInput = scnr.nextLine();
+            if (userInput.equals("esc")) break;
+            classesTaken.add(userInput.toUpperCase());
+        }
+
+        System.out.println();
+        System.out.println("You've taken...");
+        for (String value : classesTaken) System.out.println("- " + value);
+        System.out.println();
+        System.out.println("You need to take...");
+        for (String value : majorData) {
+            LinkedList<String> splitVals = CSVReader.parseCSVLine(value, ";");
+            if (splitVals.size() < 2) continue;
+            System.out.println("- " + splitVals.get(0) + " : \t" + splitVals.get(1));
+        }
 
         // Construct graph based on majors and classes taken
 
